b2c9fb36a13fd250344b4c4091d8b446c7d48840
eitoball <eitoball@gmail.com>
2010-03-08 00:14:27 +0900

    Fixed test cases and removed test cases

--- test/functional/lightning_talk_submissions_controller_test.rb
+++ test/functional/lightning_talk_submissions_controller_test.rb
@@ -3,43 +3,26 @@ require 'test_helper'
 class LightningTalkSubmissionsControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
-    assert_response :success
-    assert_not_nil assigns(:lightning_talk_submissions)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
+    assert_response :redirect
+    if lt_submission_open?
+      assert_not_nil assigns(:lightning_talk_submissions)
+    end
   end
 
   test "should create lightning_talk_submission" do
     assert_difference('LightningTalkSubmission.count') do
-      post :create, :lightning_talk_submission => { }
+      post :create, {:lightning_talk_submission => {:name => "_name_", :email => "_email_", :title => "_title_", :abstract => "_abstract_"}, :locale => "en"}
     end
 
-    assert_redirected_to lightning_talk_submission_path(assigns(:lightning_talk_submission))
+    assert_redirected_to :action => "thankyou"
   end
 
-  test "should show lightning_talk_submission" do
-    get :show, :id => lightning_talk_submissions(:one).to_param
-    assert_response :success
+  test "should get thankyou" do
+    get :thankyou
+    assert_response :redirect
   end
-
-  test "should get edit" do
-    get :edit, :id => lightning_talk_submissions(:one).to_param
-    assert_response :success
-  end
-
-  test "should update lightning_talk_submission" do
-    put :update, :id => lightning_talk_submissions(:one).to_param, :lightning_talk_submission => { }
-    assert_redirected_to lightning_talk_submission_path(assigns(:lightning_talk_submission))
-  end
-
-  test "should destroy lightning_talk_submission" do
-    assert_difference('LightningTalkSubmission.count', -1) do
-      delete :destroy, :id => lightning_talk_submissions(:one).to_param
-    end
-
-    assert_redirected_to lightning_talk_submissions_path
+  
+  def lt_submission_open?
+    Time.now <= Time.parse(configatron.lt_submission.close_at)
   end
 end

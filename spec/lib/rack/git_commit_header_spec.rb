b09bbe8c2b4416a2519131481f7ae36d08681bd0
KAKUTANI Shintaro <shintaro@kakutani.com>
2010-03-31 10:40:39 +0900

    [Fixed #2966115] reviewd

--- spec/lib/rack/git_commit_header_spec.rb
+++ spec/lib/rack/git_commit_header_spec.rb
@@ -5,24 +5,22 @@ describe Rack::GitCommitHeader do
     Rack::GitCommitHeader.new(proc {|env| [200, {}, 'hello'] }).call(nil).second
   end
 
-  context 'git returns valid revision' do
+  context 'when git returns valid revision' do
     before do
       @rev = ActiveSupport::SecureRandom.hex(20)
       mock.instance_of(Rack::GitCommitHeader).git_rev_parse_head { @rev }
     end
 
-    describe 'headers' do
-      specify { headers.should == {'X-Git-Commit' => @rev} }
-    end
+    subject { headers }
+    it { should == {'X-Git-Commit' => @rev} }
   end
 
-  context 'git returns invalid revision' do
+  context 'when git returns invalid revision' do
     before do
       mock.instance_of(Rack::GitCommitHeader).git_rev_parse_head { 'ho' }
     end
 
-    describe 'headers' do
-      specify { headers.should_not have_key('X-Git-Commit') }
-    end
+    subject { headers }
+    it { should_not have_key('X-Git-Commit') }
   end
 end

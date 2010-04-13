9c5c1ff3ff635978b473341d6a2a5a7d38dbb572
Keita Urashima <ursm@ursm.jp>
2010-04-14 01:46:48 +0900

    twitter_user_id is must present and unique.

--- app/models/rubyist.rb
+++ app/models/rubyist.rb
@@ -1,4 +1,7 @@
 class Rubyist < ActiveRecord::Base
 	validates_uniqueness_of :name
 	validates_format_of :name, :with => /[\w-]+/
+
+	validates_uniqueness_of :twitter_user_id
+	validates_presence_of :twitter_user_id
 end

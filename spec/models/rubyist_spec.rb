e59d7128f17e408a94e7470cbea4e7c035fc31e7
Keita Urashima <ursm@ursm.jp>
2010-04-14 01:45:30 +0900

    drop dorky spec

--- spec/models/rubyist_spec.rb
+++ spec/models/rubyist_spec.rb
@@ -1,16 +1,4 @@
 require 'spec_helper'
 
 describe Rubyist do
-  before(:each) do
-    @valid_attributes = {
-      :name => "value for name",
-      :email => "value for email",
-      :oauth_token => "value for oauth_token",
-      :oauth_secret => "value for oauth_secret"
-    }
-  end
-
-  it "should create a new instance given valid attributes" do
-    Rubyist.create!(@valid_attributes)
-  end
 end

00be8350f233ef68f3257559eeba5df0c358cdcd
Keita Urashima <ursm@ursm.jp>
2010-04-15 03:31:24 +0900

    [finished #3139644] some names are reserved for routing.

--- app/models/rubyist.rb
+++ app/models/rubyist.rb
@@ -1,6 +1,7 @@
 class Rubyist < ActiveRecord::Base
   validates_uniqueness_of :name
   validates_format_of :name, :with => /[\w-]+/
+  validates_exclusion_of :name, :in => %w(new)
 
   validates_uniqueness_of :twitter_user_id
   validates_presence_of :twitter_user_id
--- spec/models/rubyist_spec.rb
+++ spec/models/rubyist_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
 
 describe Rubyist do
-  describe 'to_param' do
-    subject { Rubyist.new(:name => 'ursm') }
+  describe 'ursm' do
+    subject { Rubyist.make_unsaved(:name => 'ursm') }
+    it { should be_valid }
     its(:to_param) { should == 'ursm' }
+  end
+
+  describe 'reserved name' do
+    %w(new).each do |reserved|
+      subject { Rubyist.make_unsaved(:name => reserved).tap(&:valid?) }
+      its(:errors) { should be_invalid(:name) }
+    end
   end
 end

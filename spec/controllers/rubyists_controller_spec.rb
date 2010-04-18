36432c81abdfd4d6dd506d96bb57b867441439a7
Keita Urashima <ursm@ursm.jp>
2010-04-19 00:40:49 +0900

    salvage spec

--- spec/controllers/rubyists_controller_spec.rb
+++ spec/controllers/rubyists_controller_spec.rb
@@ -1,6 +1,16 @@
 require 'spec_helper'
 
 describe RubyistsController do
+  describe 'GET /show' do
+    before do
+      @ursm = Rubyist.make(:name => 'ursm')
+      get :show, :id => 'ursm'
+    end
+
+    it { response.should be_success }
+    it { assigns[:rubyist].should == @ursm }
+  end
+
   describe 'POST /create' do
     context 'with Twitter credentials' do
       before do

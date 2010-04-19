da7bc4ad2e2bf434c2629dde3655a2a4378ac9f2
Keita Urashima <ursm@ursm.jp>
2010-04-20 02:48:00 +0900

    minor

--- spec/models/headline_entry_spec.rb
+++ spec/models/headline_entry_spec.rb
@@ -16,7 +16,7 @@ describe HeadlineEntry do
     it { HeadlineEntry.recent('ja', 7).should have(6).records }
     it { HeadlineEntry.recent('en', 7).should have(1).record }
 
-    describe 'lang: ja' do
+    describe 'lang:ja' do
       subject { HeadlineEntry.find_by_link('http://rubykaigi.tdiary.net/20100225.html#p02') }
       its(:title) { should == '日本Ruby会議2010の趣意書が公開されました' }
       its(:time) { should == Time.parse('2010-02-25T08:58:34+09:00') }
@@ -24,14 +24,14 @@ describe HeadlineEntry do
       its(:language) { should == 'ja' }
     end
 
-    describe 'lang: en' do
+    describe 'lang:en' do
       subject { HeadlineEntry.find_by_link('http://rubykaigi.tdiary.net/20100412.html#p01') }
       its(:title) { should == 'RubyKaigi 2010 is open for talk and sub-event organizers, conference sponsors and illustrations!' }
       its(:language) { should == 'en' }
     end
 
     describe 'exists' do
-      subject { HeadlineEntry.find_by_link('http://rubykaigi.tdiary.net/20100225.html#p01') }
+      subject { HeadlineEntry.find_by_link(@exists.link) }
       its(:title) { should == 'Ruby会議2010がそろそろ動き出したようです' }
     end
   end

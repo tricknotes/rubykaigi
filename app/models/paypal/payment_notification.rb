84e5ad3a9062b335814c1acc7b50968bb0934c2d
Keita Urashima <ursm@ursm.jp>
2010-04-20 20:52:11 +0900

    fixed critical errors

--- app/models/paypal/payment_notification.rb
+++ app/models/paypal/payment_notification.rb
@@ -1,16 +1,25 @@
 module Paypal
-  class PaymentNotification
+  class PaymentNotification < ActiveRecord::Base
     class Item < ActiveRecord::Base
       set_table_name "paypal_payment_notification_items"
 
-      belongs_to :paypal_payment_notification, :class => Paypal::PaymentNotification
-      alias :name :item_name
-      alias :number :item_number
-      alias :price :mc_gross
+      belongs_to :paypal_payment_notification, :class_name => Paypal::PaymentNotification.name
+
+      def name
+         item_name
+      end
+
+      def number
+         item_number
+      end
+
+      def price
+         mc_gross
+      end
     end # Item
 
     set_table_name "paypal_payment_notifications"
     belongs_to :order
-    has_many :items, :class => Paypal::PaymentNotification::Item
+    has_many :items, :class_name => Paypal::PaymentNotification::Item.name
   end
 end

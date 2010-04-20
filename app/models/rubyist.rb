adbeca99896399dfe4c9d51af72c215af6c4059f
Keita Urashima <ursm@ursm.jp>
2010-04-20 23:00:56 +0900

    [fixed #3224186] credentials are allow duplication of null.

--- app/models/rubyist.rb
+++ app/models/rubyist.rb
@@ -3,8 +3,8 @@ class Rubyist < ActiveRecord::Base
   validates_format_of :name, :with => /[\w-]+/
   validates_exclusion_of :name, :in => %w(new edit)
 
-  validates_uniqueness_of :twitter_user_id
-  validates_uniqueness_of :identity_url
+  validates_uniqueness_of :twitter_user_id, :allow_nil => true
+  validates_uniqueness_of :identity_url, :allow_nil => true
 
   attr_protected :twitter_user_id, :identity_url
 
--- spec/models/rubyist_spec.rb
+++ spec/models/rubyist_spec.rb
@@ -8,9 +8,20 @@ describe Rubyist do
   end
 
   %w(new edit).each do |reserved|
-    context "name is #{reserved}" do
+    context "reserved name: #{reserved}" do
       subject { Rubyist.make_unsaved(:name => reserved).tap(&:valid?) }
       its(:errors) { should be_invalid(:name) }
     end
+  end
+
+  context 'two rubyists' do
+    before do
+       @ursm = Rubyist.make(:name => 'ursm', :twitter_user_id => 1234)
+       @kakutani = Rubyist.make(:name => 'kakutani', :twitter_user_id => 3456)
+    end
+
+    it { Rubyist.should have(2).records }
+    it { @ursm.should be_valid }
+    it { @kakutani.should be_valid }
   end
 end

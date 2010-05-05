3c4241c1895f90ab4585c16378614656bb5e1f91
SHIMADA Koji <snoozer.05@gmail.com>
2010-05-05 10:04:45 +0900

    set 0 in initial value of the field doesn't permit null
    SQlite3 で migrate した際に NULL を認めないとかは
    初期値を設定してから言えと怒られたので追加しました

--- db/migrate/20100420155418_add_price_to_orders.rb
+++ db/migrate/20100420155418_add_price_to_orders.rb
@@ -1,6 +1,6 @@
 class AddPriceToOrders < ActiveRecord::Migration
   def self.up
-    add_column :orders, :price, :integer, :null => false
+    add_column :orders, :price, :integer, :default => 0, :null => false
   end
 
   def self.down
--- db/migrate/20100428024808_add_to_capacity_and_party_capacity_to_ruby_kaigis.rb
+++ db/migrate/20100428024808_add_to_capacity_and_party_capacity_to_ruby_kaigis.rb
@@ -1,7 +1,7 @@
 class AddToCapacityAndPartyCapacityToRubyKaigis < ActiveRecord::Migration
   def self.up
-    add_column :ruby_kaigis, :capacity, :integer, :null => false
-    add_column :ruby_kaigis, :party_capacity, :integer, :null => false
+    add_column :ruby_kaigis, :capacity, :integer, :default => 0, :null => false
+    add_column :ruby_kaigis, :party_capacity, :integer, :default => 0, :null => false
   end
 
   def self.down

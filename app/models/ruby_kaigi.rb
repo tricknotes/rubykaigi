89e1f7f6aa08f92657577ae397c2974bbed16c5f
KAKUTANI Shintaro <shintaro@kakutani.com>
2010-05-10 23:07:02 +0900

    reorganize

--- app/models/ruby_kaigi.rb
+++ app/models/ruby_kaigi.rb
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 class RubyKaigi < ActiveRecord::Base
   class << self
+    def year(year)
+      RubyKaigi.find_by_year(year)
+    end
+
     (2006..2010).to_a.each do |year|
       define_method("_#{year}") do
-        RubyKaigi.find_by_year(year)
+        RubyKaigi.year(year)
       end
     end
     alias :latest :_2010

94d3925d08f1d7d557ee8c4c21f9b4bbb9a8a11e
Keita Urashima <ursm@ursm.jp>
2010-05-13 02:05:34 +0900

    [finished #3506043] add validation to IndividualSponsorOption#link_url.

--- app/models/individual_sponsor_option.rb
+++ app/models/individual_sponsor_option.rb
@@ -7,6 +7,7 @@ class IndividualSponsorOption < ActiveRecord::Base
   validates_numericality_of :additional_amount, :greater_than_or_equal_to => 0, :only_integer => true
   validates_length_of :link_label, :maximum => 128, :allow_blank => true
   validates_length_of :link_url, :maximum => 255, :allow_blank => true
+  validates_format_of :link_url, :with => URI.regexp(%w(http https)), :allow_blank => true
 
   private
   def link_information_given?
--- spec/models/individual_sponsor_option_spec.rb
+++ spec/models/individual_sponsor_option_spec.rb
@@ -1,4 +1,28 @@
 require 'spec_helper'
 
 describe IndividualSponsorOption do
+  context 'ちゃんと入れた' do
+    subject { IndividualSponsorOption.make_unsaved }
+    it { should be_valid }
+  end
+
+  context 'additional_amount がマイナス' do
+    subject { IndividualSponsorOption.make_unsaved(:additional_amount => -1).tap(&:valid?) }
+    its(:errors) { should be_invalid(:additional_amount) }
+  end
+
+  context 'link_url が変' do
+    subject { IndividualSponsorOption.make_unsaved(:link_url => 'javascript:alert("hi!")').tap(&:valid?) }
+    its(:errors) { should be_invalid(:link_url) }
+  end
+
+  context 'link_label と link_url が両方とも空' do
+    subject { IndividualSponsorOption.make_unsaved(:link_label => '', :link_url => '') }
+    it { should be_valid }
+  end
+
+  context 'link_label と link_url が片方だけ入ってる' do
+    specify { IndividualSponsorOption.make_unsaved(:link_label => '').should_not be_valid }
+    specify { IndividualSponsorOption.make_unsaved(:link_url   => '').should_not be_valid }
+  end
 end

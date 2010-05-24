b0eb277d4be58e69230694175604e7b8e46f3513
KAKUTANI Shintaro <shintaro@kakutani.com>
2010-05-24 16:49:48 +0900

    render flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,19 @@ module ApplicationHelper
   def t_required(locale_name)
     "#{t(locale_name)}&nbsp;<span class='required'>(#{t(:required)})</span>"
   end
+
+  def render_flashes
+    if (flash_message = flash[:error])
+      level = 'error'
+    elsif (flash_message = flash[:warning])
+      level = 'warning'
+    elsif (flash_message = flash[:notice])
+      level = 'notice'
+    end
+    if (flash_message)
+      render :partial => "shared/flash", :locals => {
+        :level => level, :flash_message => flash_message}
+    end
+  end
+
 end

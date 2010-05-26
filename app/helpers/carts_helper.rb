7540937ffd1b1b05a9c1859e101129ecd6ffd485
KAKUTANI Shintaro <shintaro@kakutani.com>
2010-05-26 16:13:13 +0900

    [Finish #3498425] refine UI sreens.
    まだまだ改善の余地はあるけれども、多少はマシになった……かな？

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,2 +1,7 @@
+# -*- coding: utf-8 -*-
 module CartsHelper
+  def proceed_to_checkout_button
+    addtocart_button = (current_locale == "ja" ? image_tag("/2010/images/proceedtocheckout_ja.png", :alt => "購入手続きへ進む") : image_tag("/2010/images/proceedtocheckout_en.png", :alt => "Proceed To Checkout"))
+    link_to(addtocart_button, orders_path, :method => :post, :class => 'noborder')
+  end
 end
--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,8 @@
 module OrdersHelper
+  def checkout_paypal_button
+#    checkout_paypal_button = (current_locale == "ja" ? image_tag("/2010/images/checkout_paypal_ja.png", :alt => "PayPalでチェックアウトする") : image_tag("/2010/images/checkout_paypal_en.png", :alt => "Checkout via PayPal"))
+#    link_to(addtocart_button, orders_path, :method => :post, :class => 'noborder')
+    checkout_paypal_button = (current_locale == "ja" ? "/2010/images/checkout_paypal_ja.png" : "/2010/images/checkout_paypal_en.png")
+    image_submit_tag(checkout_paypal_button)
+  end
 end

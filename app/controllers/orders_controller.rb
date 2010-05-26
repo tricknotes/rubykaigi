7280dd75e6bb4ecd52280ec714b41d54f60f32ef
KAKUTANI Shintaro <shintaro@kakutani.com>
2010-05-26 20:18:17 +0900

    oops

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -69,9 +69,9 @@ class OrdersController < ApplicationController
     @option.attend_party = opt_params[:attend_party]
     @option.link_label = opt_params[:link_label]
     @option.link_url = opt_params[:link_url]
-    order = @option.order
+    order = @option.order_item.order
     order.calculate_price
-    Option.transaction do
+    Order.transaction do
       @option.save
       order.save
     end

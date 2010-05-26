1a42021b9998379d241d5a6471ef7f2313a494df
KAKUTANI Shintaro <shintaro@kakutani.com>
2010-05-26 17:18:15 +0900

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ rk10 = RubyKaigi.create(:year => 2010, :capacity => 900, :party_capacity => 310,
   :registration_open_at => DateTime.parse('2010-05-26 18:59:59'))
 ProductItem.create(:item_code => 'rk10', :unit_price => 6000, :stock => 700, :ruby_kaigi => rk10,
   :open_sale_at => DateTime.parse('2010-06-15 19:00:00'), :ready_for_sale => true)
-ProductItem.create(:item_code => 'rk10_party', :unit_price => 5000, :stock => 150, :ruby_kaigi => rk10,
+ProductItem.create(:item_code => 'rk10_party', :unit_price => 5000, :stock => 50, :ruby_kaigi => rk10,
   :open_sale_at => DateTime.parse('2010-05-26 18:59:59'), :ready_for_sale => true)
 ProductItem.create(:item_code => 'rk10_individual_sponsor', :unit_price => 20000, :stock => 100, :ruby_kaigi => rk10,
-  :open_sale_at => DateTime.parse('2010-05-07 19:00:00'), :ready_for_sale => true)
+  :open_sale_at => DateTime.parse('2010-05-26 18:59:59'), :ready_for_sale => true)

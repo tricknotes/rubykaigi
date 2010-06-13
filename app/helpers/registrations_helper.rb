d66ade0920ae73acd6cb8a7af5e9a6a468dd2032
KAKUTANI Shintaro <shintaro@kakutani.com>
2010-06-13 20:46:46 +0900

    extract add_cart_button to partial.

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -1,2 +1,7 @@
 module RegistrationsHelper
+  def img_add_to_cart_button
+    file_path = "/2010/images/addtocart_#{current_locale}.png"
+    alt = (current_locale == "ja" ? "カートへ入れる" : "Add To Cart")
+    image_tag(file_path, :alt => alt)
+  end
 end

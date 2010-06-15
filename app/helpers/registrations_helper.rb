bcf019ec21b0e87cd1738946e436d382f1ff6139
KAKUTANI Shintaro <shintaro@kakutani.com>
2010-06-15 14:00:22 +0900

    [Finish #3324221] show product item stock information.

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -4,4 +4,21 @@ module RegistrationsHelper
     alt = (current_locale == "ja" ? "カートへ入れる" : "Add To Cart")
     image_tag(file_path, :alt => alt)
   end
+
+  def stock_notification(item)
+    stock = item.stock
+    if stock == 0
+      content_tag :span, :style => 'background:#e7161c;border:1px solid #999;padding: 4px;margin-left:1em' do
+        'Out of Stock'
+      end
+    elsif stock < 20
+      content_tag :span, :style => 'background:#d5d536;border:1px solid #999;padding: 4px;margin-left:1em' do
+        'Running Out of Stock'
+      end
+    else
+      content_tag :span, :style => 'background:#36d536;border:1px solid #999;padding: 4px;margin-left:1em' do
+        'In Stock'
+      end
+    end
+  end
 end

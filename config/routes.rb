49764b3d78e0d767eea5f95c030311220c690d30
KAKUTANI Shintaro <shintaro@kakutani.com>
2010-06-16 11:47:44 +0900

    add alias

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :events, :path_prefix => ":year/:locale", :requirements => {:year => /2\d{3}/, :locale => /en|ja/}
 
+  map.signin "/signin", :controller => 'sessions', :action => 'new'
+
   map.resource :sessions, :collection => {:unauthenticated => :get}
   map.resource :account
   map.resources :rubyists

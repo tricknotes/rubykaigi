838da63dcbe39c101641319101fd6cdd279ad533
KAKUTANI Shintaro <shintaro@kakutani.com>
2010-07-16 15:39:44 +0900

    adjust timetable stylesheet.
    - ホールのほうを幅を広く、企画の部屋を幅は狭く、ホワイエはもっと狭くしたかったけど、だいたい狭いのでこんなもんかな
    - Room#hall? はとにかく手っ取り早くやってみただけ。

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,9 +1,19 @@
+# -*- coding: utf-8 -*-
 class Room < ActiveRecord::Base
   has_many :event_rooms
   has_many :events, :through => :event_rooms
 
   def name
     I18n.locale.to_s == 'ja' ? (name_ja || name_en) : (name_en || name_ja)
+  end
+
+  def hall?
+    case I18n.locale.to_s
+    when 'ja'
+      name.match(/ホール/u)
+    else
+      name.match(/hall/i)
+    end
   end
 
   def empty_at?(time)

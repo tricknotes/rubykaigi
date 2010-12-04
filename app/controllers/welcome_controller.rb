fdbdb2964153f32e405aa1d8b7d96a69ebe8f944
Kakutani Shintaro <shintaro@kakutani.com>
2010-12-04 11:24:35 +0900

    redirect to /2011 from toppage (tentative)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
 class WelcomeController < LocaleBaseController
   def index
-    redirect_to "/#{RubyKaigi.latest_year}"
+# latest_yearをイジるとPayPalまわりが壊れるので、決め打ち
+#    redirect_to "/#{RubyKaigi.latest_year}"
+    redirect_to "/2011"
   end
 end

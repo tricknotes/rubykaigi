979a4387a0a0100b97c95cf8eea533163d09456c
Eito Katagiri <eitoball@gmail.com>
2011-01-25 22:25:30 +0900

    add slide materials for 28th

--- db/seeds_20100828.rb
+++ db/seeds_20100828.rb
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 EventLoader.with_options(:day => 28) do |e28|
   e28.with_options(:room => @big) do |e28_big|
-     e28_big.create :title => 'Ruby 1.9.2 is released! / Ruby committers Q & A', :title_ja => 'Ruby 1.9.2 is released! / Rubyコミッタ Q & A', :speaker => 'Yugui, Shugo Maeda', :from => '9:30', :to => '11:00',:abstract => (<<ABSTRACT), :abstract_ja => (<<ABSTRACT_JA), :lang => 'ja'
+     e28_big.create :title => 'Ruby 1.9.2 is released! / Ruby committers Q & A', :title_ja => 'Ruby 1.9.2 is released! / Rubyコミッタ Q & A', :speaker => 'Yugui, Shugo Maeda', :from => '9:30', :to => '11:00',:abstract => (<<ABSTRACT), :abstract_ja => (<<ABSTRACT_JA), :lang => 'ja', :presentation_material => (<<PRESENTATION_MATERIAL)
 Many committers will come to RubyKaigi, so it's a good chance to ask them. In this event Ruby committers will answer questions from audience (or other committers).
 ABSTRACT
 RubyKaigiにはたくさんのコミッタが来場するので、彼らに質問するよい機会です。このイベントでは、Rubyコミッタが聴衆(又は他のコミッタ) からの質問に回答します。
 ABSTRACT_JA
+http://www.slideshare.net/yugui/ruby-192-is-released
+PRESENTATION_MATERIAL
     e28_big.create :title => 'Keynote', :title_ja => '基調講演', :speaker => 'Matz', :length => 1.hour, :lang => 'ja', :profile_ja => (<<PROFILE_JA)
 いまや数えきれないほどのユーザーを持つ世界的プログラミング言語Rubyの創造者。っていうか、そういう立場に本人が一番困惑してるんですが。
 PROFILE_JA
@@ -26,18 +28,20 @@ Esoteric programming や Quine を好む。
 PROFILE_JA
 http://www.slideshare.net/mametter/ruby-esoteric-obfuscated-ruby-programming-5088683
 PRESENTATION_MATERIAL
-    e28_big.create :title => 'Daily Ruby', :title_ja => 'Rubyな日々', :speaker => 'Kazuhiro NISHIYAMA (Good-Day, Inc.)',:abstract => (<<ABSTRACT), :abstract_ja => (<<ABSTRACT_JA), :lang => 'ja'
+    e28_big.create :title => 'Daily Ruby', :title_ja => 'Rubyな日々', :speaker => 'Kazuhiro NISHIYAMA (Good-Day, Inc.)',:abstract => (<<ABSTRACT), :abstract_ja => (<<ABSTRACT_JA), :lang => 'ja', :presentation_material => (<<PRESENTATION_MATERIAL)
 I will talk about my daily life as a committer of Ruby and the Ruby Reference Manual Renewal Project.
 ABSTRACT
 Ruby や Ruby リファレンスマニュアル刷新計画のコミッターとしてどういうことをしているのかという話をする予定です。
 ABSTRACT_JA
+http://www.n-z.jp/presen/RubyKaigi2010/DailyRuby.pdf
+PRESENTATION_MATERIAL
     e28_big.create :title => 'Ruby Reference Manual Renewal Project 2010 Summer', :title_ja => 'Ruby リファレンスマニュアル刷新計画 2010 夏', :speaker => 'okkez (Ruby Reference Manual Renewal Project)',:abstract => (<<ABSTRACT), :abstract_ja => (<<ABSTRACT_JA), :lang => 'ja'
 In this talk, I will talk about Ruby Reference Manual Renewal Project and discuss the latest developments and future plans.
 ABSTRACT
 昨年の報告以降の動きをまとめて報告します。また、今後の予定や特に助けが必要な部分について説明します。
 ABSTRACT_JA
     e28_big.create_break
-    e28_big.create :title => 'Ruby API is Improved Unix API', :title_ja => 'Unix修正主義', :speaker => 'Tanaka Akira (National Institute of Advanced Industrial Science and Technology (AIST))', :length => 1.hour,:abstract => (<<ABSTRACT), :abstract_ja => (<<ABSTRACT_JA), :lang => 'ja', :profile => (<<PROFILE), :profile_ja => (<<PROFILE_JA)
+    e28_big.create :title => 'Ruby API is Improved Unix API', :title_ja => 'Unix修正主義', :speaker => 'Tanaka Akira (National Institute of Advanced Industrial Science and Technology (AIST))', :length => 1.hour,:abstract => (<<ABSTRACT), :abstract_ja => (<<ABSTRACT_JA), :lang => 'ja', :profile => (<<PROFILE), :profile_ja => (<<PROFILE_JA), :presentation_material => (<<PRESENTATION_MATERIAL)
 Ruby's library API is like Unix's API, but improved. Ruby tries to fix some of the problems in Unix's API. For example, non-blocking IO in Unix is provided by setting a flag when opening a file, making it difficult to specify non-blocking behavior for individual operations. Ruby overcomes this problem by providing non-blocking methods such as #read_nonblock.
 
 Another example is the fork() system call, which has problems in multi-threaded environments. Ruby provides the #spawn method as an alternative way to launch processes safely.
@@ -52,6 +56,8 @@ PROFILE
 Ruby コミッタ。
 主に I/O、ソケット、プロセス、時刻、その他の厄介事に興味を持つ。
 PROFILE_JA
+http://www.a-k-r.org/pub/rubykaigi2010-akr-unix-improved.pdf
+PRESENTATION_MATERIAL
     e28_big.create_break
     lt_event = e28_big.create :title => 'Lightning Talks', :abstract => "Lightning Talks are 11 x 5minutes talk in a 60 minutes time slot", :abstract_ja => "1人持ち時間5分のライトニングトークです", :speaker => '(See Abstract)', :length => 1.hour, :lang => 'ja, en'
     e28_big.create :title => 'ARToolKit Ruby Binding', :abstract => '', :speaker => 'Urabe, Shyouhei (Network Applied Communication Lab.)', :lang => 'ja', :parent_event => lt_event, :from => '17:00', :to => '18:00', :profile => (<<PROFILE), :profile_ja => (<<PROFILE_JA)
@@ -71,22 +77,26 @@ CRuby の commit 権所有者で，Ruby/Tk 開発者．
 PROFILE_JA
 http://www.dumbo.ai.kyutech.ac.jp/~nagai/RubyTk/?%A5%D7%A5%EC%A5%BC%A5%F3%A5%C6%A1%BC%A5%B7%A5%E7%A5%F3%BB%F1%CE%C1
 PRESENTATION_MATERIAL
-    e28_big.create :title => 'What is few?', :abstract => '', :speaker => 'Shota Fukumori/@sora_h (few developers team)', :lang => 'ja', :parent_event => lt_event, :from => '17:00', :to => '18:00', :profile => (<<PROFILE), :profile_ja => (<<PROFILE_JA)
+    e28_big.create :title => 'What is few?', :abstract => '', :speaker => 'Shota Fukumori/@sora_h (few developers team)', :lang => 'ja', :parent_event => lt_event, :from => '17:00', :to => '18:00', :profile => (<<PROFILE), :profile_ja => (<<PROFILE_JA), :presentation_material => (<<PRESENTATION_MATERIAL)
 Shota Fukumori (sora_h) is a junior high school 2 grade *half-time(!)* student and a rubyist in Japan.
 He commits to Termtter and some projects and his repository, and create web service by rails. Also he is usually talking at some communities. He has a online workshop which name is online.sg http://onlinesg.org/ .
 PROFILE
 Shota Fukumori (sora_h)は中2で半登校 (昼休み後早退). よくtermtterや自分のレポジトリにコミットしたり, Rails などでいろいろ作ってたり, よくいろんなコミュニティのチャットで喋っている. そしてOnline.sg と呼ばれるオンラインの勉強会を開催したりもしている.
 PROFILE_JA
-    e28_big.create :title => 'Toward Lightning RubyVM', :abstract => '', :speaker => 'Koichi Sasada (The University of Tokyo)', :lang => 'ja', :parent_event => lt_event, :from => '17:00', :to => '18:00'
-    e28_big.create :title => 'MessagePack, a cross-language communication system', :title_ja => 'MessagePackで多言語間通信', :speaker => 'Sadayuki Furuhashi', :lang => 'ja', :parent_event => lt_event, :from => '17:00', :to => '18:00', :abstract => '', :abstract_ja => ''
+http://www.slideshare.net/sorah/what-is-few
+PRESENTATION_MATERIAL
+    e28_big.create :title => 'Toward Lightning RubyVM', :abstract => '', :speaker => 'Koichi Sasada (The University of Tokyo)', :lang => 'ja', :parent_event => lt_event, :from => '17:00', :to => '18:00', :presentation_material => 'http://rvm.jp/~ko1/activities/rk2010-lt-ko1.pdf'
+    e28_big.create :title => 'MessagePack, a cross-language communication system', :title_ja => 'MessagePackで多言語間通信', :speaker => 'Sadayuki Furuhashi', :lang => 'ja', :parent_event => lt_event, :from => '17:00', :to => '18:00', :abstract => '', :abstract_ja => '', :presentation_material => 'http://syuki.skr.jp/files/20100728/rubykaigi2010-msgpack.pdf'
     e28_big.create :title => 'Let\'s calculate easily civil days by using "Ruby"', :title_ja => 'Rubyで手軽に暦日を算出しよう！', :abstract => '',:abstract_ja => '' ,:speaker => 'Yoshihiko Hara, Japan OSS Promotion Forum Application Sub Committee Ruby Application Task Force, FUJITSU SOCIAL SCIENCE LABORATORY LIMITED', :lang => 'ja', :parent_event => lt_event, :from => '17:00', :to => '18:00'
-    e28_big.create :title => 'Introducing the Lingo Project: A New Generationi Text Input System Leveraging Non-native English Writing', :abstract => '', :speaker => 'Kazki Matz (Lingo project)', :lang => 'ja', :parent_event => lt_event, :from => '17:00', :to => '18:00'
-    e28_big.create :title => 'babushka―test-driven sysadmin for rubyists', :abstract => '', :speaker => 'Ben Hoskings (babushka)', :parent_event => lt_event, :from => '17:00', :to => '18:00'
-    e28_big.create :title => 'parse.y Fam Tour', :title_ja => "parse.yの歩き方 -ワシのRubyは4式まであるぞ-", :abstract => '', :speaker => 'Ando Yasushi (Seesaa Inc.)', :lang => 'ja', :parent_event => lt_event, :from => '17:00', :to => '18:00', :profile => (<<PROFILE), :profile_ja => (<<PROFILE_JA)
+    e28_big.create :title => 'Introducing the Lingo Project: A New Generationi Text Input System Leveraging Non-native English Writing', :abstract => '', :speaker => 'Kazki Matz (Lingo project)', :lang => 'ja', :parent_event => lt_event, :from => '17:00', :to => '18:00', :presentation_material => 'http://www.slideshare.net/KazkiMatz/introducing-the-lingo-project'
+    e28_big.create :title => 'babushka―test-driven sysadmin for rubyists', :abstract => '', :speaker => 'Ben Hoskings (babushka)', :parent_event => lt_event, :from => '17:00', :to => '18:00', :presentation_material => 'http://www.slideshare.net/benhoskings/rubykaigi-2010-babushka-lightning-talk'
+    e28_big.create :title => 'parse.y Fam Tour', :title_ja => "parse.yの歩き方 -ワシのRubyは4式まであるぞ-", :abstract => '', :speaker => 'Ando Yasushi (Seesaa Inc.)', :lang => 'ja', :parent_event => lt_event, :from => '17:00', :to => '18:00', :profile => (<<PROFILE), :profile_ja => (<<PROFILE_JA), :presentation_material => (<<PRESENTATION_MATERIAL)
 The author of the first and last Google Wave book written in Japanese; the book released on August 19th.
 PROFILE
 たぶん史上最後のGoogle Wave本の著者。あとおっぱいとかカリスマとか
 PROFILE_JA
+https://docs.google.com/present/view?id=dchb3tb2_201fz7kq3xf
+PRESENTATION_MATERIAL
     e28_big.create :title => 'The Timeless Way of E-Publishing with Ruby', :title_ja => "時を超えた電子出版の道の中をRubyと歩いていく", :abstract => '', :speaker => 'Masayoshi Takahashi (Tatsu-zine publishing, Nihon Ruby-no-Kai)', :lang => 'ja', :parent_event => lt_event, :from => '17:00', :to => '18:00'
     e28_big.create :title => "My Report of \"Ruby Summer of Code 2010\" - Road to be a stdlib -", :title_ja => "Ruby Summer of Code 2010のご報告 〜俺たちのDecimalはまだ始まったばかりだ〜", :abstract => '', :speaker => 'Tadashi Saito (University of Tsukuba)', :lang => 'ja', :parent_event => lt_event, :from => '17:00', :to => '18:00', :profile => (<<PROFILE)
 Tadashi is in his nth year of University of Tsukuba where n is smaller than 0xDEAD. He is always interested in to make the world better a little, so he started Decimal project. It's the only one project which was accepted in Ruby Summer of Code 2010 from Japan, but Tadashi wants a cute girlfriend rather than a bounty. His weak subject is math.
@@ -94,17 +104,23 @@ PROFILE
   end
 
   e28.with_options(:room => @medium) do |e28_medium|
-    e28_medium.create :title => 'Rocking the enterprise with Ruby', :speaker => 'Munjal Budhabhatti And Sudhindra Rao (ThoughtWorks Inc)', :from => '9:30',:abstract => (<<ABSTRACT)
+    e28_medium.create :title => 'Rocking the enterprise with Ruby', :speaker => 'Munjal Budhabhatti And Sudhindra Rao (ThoughtWorks Inc)', :from => '9:30',:abstract => (<<ABSTRACT), :presentation_material => (<<PRESENTATION_MATERIAL)
 ThoughtWorks has been working on a long scale project at Rackspace building data center of the future using Ruby. This projects has evolved since its inception. And so has the architecture. Developing in ruby has assisted us in managing changes to the business rules at a rapid pace. We have been delivering successfully every two weeks for last three years. We would like to share our learnings from this effort.
 ABSTRACT
-    e28_medium.create :title => 'Rails to Sinatra: What is ready', :speaker => 'Jiang Wu (Tengu)',:abstract => (<<ABSTRACT), :lang => 'en', :profile => (<<PROFILE)
+http://ruote.s3.amazonaws.com/rocking_the_enterprise_with_ruby.pdf
+PRESENTATION_MATERIAL
+    e28_medium.create :title => 'Rails to Sinatra: What is ready', :speaker => 'Jiang Wu (Tengu)',:abstract => (<<ABSTRACT), :lang => 'en', :profile => (<<PROFILE), :presentation_material => (<<PRESENTATION_MATERIAL)
 As Sinatra 1.0 came out, we have a good alternative to rails(before is merb). I will introduce efforts of people around Sinatra community to eliminate gap of moving from Rails to Sinatra including my works.
 ABSTRACT
 Jiang Wu(呉江) is a Ruby developer in Shanghai, China. He has worked with Ruby for 3 years. In 2008, he open sourced a library of Chinese payment gateway, implemented in Active Merchant. From 2009, he is devoted to promote "off rails", such as Sinatra and Sequel, in China.
 PROFILE
-    e28_medium.create :title => 'Mapping the world with DataMapper', :speaker => 'Ted Han (%w(Videojuicer DataMapper))',:abstract => (<<ABSTRACT)
+http://www.slideshare.net/jiang.wu/sinatra-and-friends
+PRESENTATION_MATERIAL
+    e28_medium.create :title => 'Mapping the world with DataMapper', :speaker => 'Ted Han (%w(Videojuicer DataMapper))',:abstract => (<<ABSTRACT), :presentation_material => (<<PRESENTATION_MATERIAL)
 Ubiquitous computing has opened the door to two new revolutions, data collection and data manipulation. Mobile phones, digital cameras, low cost electronics, and wireless internet access have made it possible to collect data any time, anywhere. But what are we doing with the data once it's collected? How do we navigate the oceans of data we generate daily? Where can we find a Rubyist's tools for data mining and manipulation? We'll discuss how to use DataMapper, a Ruby data access library, to make sense of data of all kinds.
 ABSTRACT
+http://www.slideshare.net/knowtheory/mapping-the-world-with-datamapper
+PRESENTATION_MATERIAL
     e28_medium.create_break :from => '12:00', :length => 90.minutes
     e28_medium.create :title => 'The Necessity and Implementation of Speedy Tests', :speaker => 'Jake Scruggs (Backstop Solutions)', :length => 1.hour,:abstract => (<<ABSTRACT), :profile => (<<PROFILE)
 Your project's software practices are deteriorating every minute the developers wait for slow tests to finish. Once developers get fed up and stop running the tests, disregard for failing builds can't be far behind, and from there it's only a short leap to the albatross of a brittle/irrelevant test suite. Luckily this can all be avoided by adopting some time-saving testing standards and practices.
@@ -117,11 +133,13 @@ http://metric-fu.rubyforge.org/
 http://twitter.com/jakescruggs
 http://www.backstopsolutions.com/
 PROFILE
-    e28_medium.create :title => 'Seamless Integration Testing', :speaker => 'paulelliott (Hashrocket)', :abstract => (<<ABSTRACT), :profile => (<<PROFILE)
+    e28_medium.create :title => 'Seamless Integration Testing', :speaker => 'paulelliott (Hashrocket)', :abstract => (<<ABSTRACT), :profile => (<<PROFILE), :presentation_material => (<<PRESENTATION_MATERIAL)
 Do you have a solid, full coverage integration test suite? You should. It is the final automated point of assurance that what you are about to push to production is not going to blow something up and cost your or your client money. I show how to integration test a real Rails application, demonstrating best practices you can apply to any setup and the latest technologies for fully covering your app, including javascript!
 ABSTRACT
 Paul works for Hashrocket on projects for clients ranging from small start ups to large enterprises. In addition to his consulting work, he contributes regularly to open source projects and maintains some of his own, most notably Fabrication, an object generation framework for Ruby. He is passionate about outside-in testing, Ruby, jQuery, and motorcycles.
 PROFILE
+http://www.slideshare.net/paulelliott99/seamless-integration-testing
+PRESENTATION_MATERIAL
     e28_medium.create_break
     e28_medium.create :title => 'A Metaprogramming Spell Book', :speaker => 'Paolo "Nusco" Perrotta', :length => 1.hour, :abstract => (<<ABSTRACT)
 When I started to learn Ruby, I was awed by the code of experienced rubyists. That code was full of amazing magic tricks that I could barely understand. People called those tricks \"metaprogramming\".

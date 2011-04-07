0de87fb6a03f4e5b4fe57b445c6231d3c6429b43
Ryunosuke SATO <tricknotes@yahoo.co.jp>
2011-04-07 15:11:42 +0900

    add files for boot up

---
+++ config/application.rb
@@ -0,0 +1,12 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# If you have a Gemfile, require the gems listed there, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(:default, Rails.env) if defined?(Bundler)
+
+module Rubykaigi
+  class Application < ::Rails::Application
+  end
+end

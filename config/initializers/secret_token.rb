f9244669d41252068e2559e9577dcf0cbbfce2b0
Ryunosuke SATO <tricknotes@yahoo.co.jp>
2011-04-07 15:49:18 +0900

    rewrite application config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubykaigi::Application.config.secret_token = 'b5210ec02dad554b71deb9849c7ba01f9d5c49c11b490488f474f8031057066faa725b6338c759cb8567734e4be425bc646da34049f220251a43f8c7aa6d114d'

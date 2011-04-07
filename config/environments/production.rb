ebbd85d6c31d88a9de245c951061df044fdab16e
Ryunosuke SATO <tricknotes@yahoo.co.jp>
2011-04-07 16:05:14 +0900

    rewrite each environment config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,39 +1,40 @@
 # Settings specified here will take precedence over those in config/environment.rb
+Rubykaigi::Application.configure do
+  # The production environment is meant for finished, "live" apps.
+  # Code is not reloaded between requests
+  config.cache_classes = true
 
-# The production environment is meant for finished, "live" apps.
-# Code is not reloaded between requests
-config.cache_classes = true
+  # Enable threaded mode
+  # config.threadsafe!
 
-# Enable threaded mode
-# config.threadsafe!
+  # Use a different logger for distributed setups
+  # config.logger = SyslogLogger.new
 
-# Use a different logger for distributed setups
-# config.logger = SyslogLogger.new
+  # Full error reports are disabled and caching is turned on
+  config.action_controller.consider_all_requests_local = false
+  config.action_controller.perform_caching             = true
 
-# Full error reports are disabled and caching is turned on
-config.action_controller.consider_all_requests_local = false
-config.action_controller.perform_caching             = true
+  # Use a different cache store in production
+  # config.cache_store = :mem_cache_store
 
-# Use a different cache store in production
-# config.cache_store = :mem_cache_store
+  # Enable serving of images, stylesheets, and javascripts from an asset server
+  # config.action_controller.asset_host                  = "http://assets.example.com"
 
-# Enable serving of images, stylesheets, and javascripts from an asset server
-# config.action_controller.asset_host                  = "http://assets.example.com"
+  # Disable delivery errors, bad email addresses will be ignored
+  # config.action_mailer.raise_delivery_errors = false
 
-# Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
+  config.after_initialize do
+    ActionMailer::Base.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => "rubykaigi.org",
+      :authentication => :login,
+      :user_name => configatron.gmail.user_name,
+      :password => configatron.gmail.password
+    }
+  end
 
-config.after_initialize do
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :domain => "rubykaigi.org",
-    :authentication => :login,
-    :user_name => configatron.gmail.user_name,
-    :password => configatron.gmail.password
-  }
+  ActionController::Base.perform_caching = true
+  ActionController::Base.cache_store = :file_store, File.join(Rails.root, 'tmp', 'rails-cache')
 end
-
-ActionController::Base.perform_caching = true
-ActionController::Base.cache_store = :file_store, File.join(Rails.root, 'tmp', 'rails-cache')
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,25 +1,26 @@
 # Settings specified here will take precedence over those in config/environment.rb
+Rubykaigi::Application.configure do
+  # The test environment is used exclusively to run your application's
+  # test suite.  You never need to work with it otherwise.  Remember that
+  # your test database is "scratch space" for the test suite and is wiped
+  # and recreated between test runs.  Don't rely on the data there!
+  config.cache_classes = false
 
-# The test environment is used exclusively to run your application's
-# test suite.  You never need to work with it otherwise.  Remember that
-# your test database is "scratch space" for the test suite and is wiped
-# and recreated between test runs.  Don't rely on the data there!
-config.cache_classes = false
+  # Log error messages when you accidentally call methods on nil.
+  config.whiny_nils = true
 
-# Log error messages when you accidentally call methods on nil.
-config.whiny_nils = true
+  # Show full error reports and disable caching
+  config.action_controller.consider_all_requests_local = true
+  config.action_controller.perform_caching             = false
 
-# Show full error reports and disable caching
-config.action_controller.consider_all_requests_local = true
-config.action_controller.perform_caching             = false
+  # Disable request forgery protection in test environment
+  config.action_controller.allow_forgery_protection    = false
 
-# Disable request forgery protection in test environment
-config.action_controller.allow_forgery_protection    = false
+  # Tell Action Mailer not to deliver emails to the real world.
+  # The :test delivery method accumulates sent emails in the
+  # ActionMailer::Base.deliveries array.
+  config.action_mailer.delivery_method = :test
 
-# Tell Action Mailer not to deliver emails to the real world.
-# The :test delivery method accumulates sent emails in the
-# ActionMailer::Base.deliveries array.
-config.action_mailer.delivery_method = :test
-
-config.logger = Logger.new("#{Rails.root}/log/#{Rails.env}.log", 50, 1048576)
-require 'pp'
+  config.logger = Logger.new("#{Rails.root}/log/#{Rails.env}.log", 50, 1048576)
+  require 'pp'
+end
